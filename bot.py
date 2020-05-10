28c0a44bd5b5b54662c408d2058a538a680ab0c6
Amaan <aslamamaan3@gmail.com>
2020-05-10 00:03:15 -0700

    without if

--- bot.py
+++ bot.py
@@ -10,7 +10,35 @@ app = Flask(__name__)
 def hello():
 		return "Hello World"
 
+@app.route("/covidbot", methods=['POST'])
 
+def covidBot():
+	receivedMsg = request.values.get('Body', '')
+	response = MessagingResponse()
+	message = response.message()
+	responded = False
+
+	
+
+	country = receivedMsg[14:]
+	country = country.strip()
+
+	r = request.get('https://coronavirus-19-api.herokuapp.com/countries/' + country)
+
+	if r.status_code = 200
+		data = json.loads(r)
+		msgText = 'Country: ' +data["country"] + '\nTotal Cases: ' +data["cases"] + '\nCases Today: ' +data["todayCases"] + '\n\nTotal Deaths' +data["deaths"]+ '\nDeaths Today' +data["todayDeaths"]+ '\nRecovered:' + data["recovered"] 		
+		message.body(msgText)
+	else
+		message.body('Service unavailable at this time')
+
+	responded = True
+
+
+	if responded == False
+		return
+
+	return str(response)	
 
 
 
