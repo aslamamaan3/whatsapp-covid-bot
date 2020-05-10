9227c38f8bdffb0099d7ec5909fe5dd9b138dc1c
Amaan <aslamamaan3@gmail.com>
2020-05-10 00:08:54 -0700

    fixed if else syntax

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def hello():
 		return "Hello World"
 
-@app.route("/covidbot", methods=['POST'])
+@@app.route("/covidbot", methods=['POST'])
 
 def covidBot():
 	receivedMsg = request.values.get('Body', '')
@@ -18,27 +18,27 @@ def covidBot():
 	message = response.message()
 	responded = False
 
-	
+	if receivedMsg.find('covid country ', 0, 13) != -1:
 
-	country = receivedMsg[14:]
-	country = country.strip()
+		country = receivedMsg[14:]
+		country = country.strip()
 
-	r = request.get('https://coronavirus-19-api.herokuapp.com/countries/' + country)
+		r = request.get('https://coronavirus-19-api.herokuapp.com/countries/' + country)
 
-	if r.status_code = 200
-		data = json.loads(r)
-		msgText = 'Country: ' +data["country"] + '\nTotal Cases: ' +data["cases"] + '\nCases Today: ' +data["todayCases"] + '\n\nTotal Deaths' +data["deaths"]+ '\nDeaths Today' +data["todayDeaths"]+ '\nRecovered:' + data["recovered"] 		
-		message.body(msgText)
-	else
-		message.body('Service unavailable at this time. Please Try Later')
+		if r.status_code = 200:
+			data = json.loads(r)
+			msgText = 'Country: ' +data["country"] + '\nTotal Cases: ' +data["cases"] + '\nCases Today: ' +data["todayCases"] + '\n\nTotal Deaths' +data["deaths"]+ '\nDeaths Today' +data["todayDeaths"]+ '\nRecovered:' + data["recovered"] 		
+			message.body(msgText)
+		else:
+			message.body('Service unavailable at this time')
 
-	responded = True
+		responded = True
 
 
 	if responded == False
 		return
 
-	return str(response)	
+	return str(response)
 
 
 
