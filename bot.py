735270350fb51a95a3b7a6af64b191af38a18c98
Amaan <aslamamaan3@gmail.com>
2020-05-10 02:19:43 -0700

    added regex for command

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from flask import request
 from twilio.twiml.messaging_response import MessagingResponse
 import requests
 import json
+import re
 
 app = Flask(__name__)
 
@@ -14,31 +15,38 @@ def hello():
 @app.route("/covidbot", methods=['POST'])
 
 def covidBot():
-	receivedMsg = request.values.get('Body', '')
+	receivedMsg = request.values.get('Body', '').lower()
 	response = MessagingResponse()
 	message = response.message()
 	responded = False
 
-	
+	regexTest = re.search("^covid country ", receivedMsg)
 
-	country = receivedMsg
-	#country = country.strip()
+	if regexTest != None:
 
-	r = requests.get('https://coronavirus-19-api.herokuapp.com/countries/' + country)
+		country = receivedMsg[13:]
+		country = country.strip()
+		print(country)
 
-	if r.status_code == 200:
+		r = requests.get('https://coronavirus-19-api.herokuapp.com/countries/' + country)
 
-		try:
-			data = r.json()
-			msgText = f'Country: *{data["country"]}* \nTotal Cases: *{data["cases"]}* \nCases Today: *{data["todayCases"]}* \n\nTotal Deaths *{data["deaths"]}* \nDeaths Today *{data["todayDeaths"]}* \n\nRecovered: *{data["recovered"]}*'
-			message.body(msgText)
-		except ValueError as e:
-			message.body('Country not found.')
+		if r.status_code == 200:
+
+			try:
+				data = r.json()
+				msgText = f'Country: *{data["country"]}* \nTotal Cases: *{data["cases"]}* \nCases Today: *{data["todayCases"]}* \n\nTotal Deaths *{data["deaths"]}* \nDeaths Today *{data["todayDeaths"]}* \n\nRecovered: *{data["recovered"]}*'
+				message.body(msgText)
+			except ValueError as e:
+				message.body('Country not found.')
+
+		else:
+			message.body('Service unavailable at this time.')
+
+		responded = True
 
 	else:
-		message.body('Service unavailable at this time.')
-
-	responded = True
+		message.body('Wrong Command. Please use - covid country countryname')
+		responded = True	
 
 
 	if responded == False:
