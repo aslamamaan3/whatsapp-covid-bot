c2b79d7ad4cf066897782f5e338b5e67fe2d7788
Amaan <aslamamaan3@gmail.com>
2020-05-10 01:19:50 -0700

    working version 1 with no string manipulation 2

--- bot.py
+++ bot.py
@@ -28,8 +28,8 @@ def covidBot():
 
 	if r.status_code == 200:
 		data = r.json()
-		msgText = f'Country: *{data["country"]}* \nTotal Cases: *{data["cases"]}* \nCases Today: *{data["todayCases"]}* \n\nTotal Deaths *{data["deaths"]}* \nDeaths Today *{data["todayDeaths"]}* \nRecovered: *{data["recovered"]}*'
-		message.body(msgText)
+		#msgText = f'Country: *{data["country"]}* \nTotal Cases: *{data["cases"]}* \nCases Today: *{data["todayCases"]}* \n\nTotal Deaths *{data["deaths"]}* \nDeaths Today *{data["todayDeaths"]}* \n\nRecovered: *{data["recovered"]}*'
+		message.body(data)
 	else:
 		message.body('Service unavailable at this time')
 
