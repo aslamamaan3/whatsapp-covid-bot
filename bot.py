512e6a3dedc832565232aeeb19deb18e0367c593
Amaan <aslamamaan3@gmail.com>
2020-05-09 23:59:20 -0700

    without /bot

--- bot.py
+++ bot.py
@@ -11,35 +11,7 @@ def hello():
 		return "Hello World"
 
 
-@app.route("/covidbot", methods=['POST'])
 
-def covidBot():
-	receivedMsg = request.values.get('Body', '')
-	response = MessagingResponse()
-	message = response.message()
-	responded = False
-
-	if receivedMsg.find('covid country ', 0, 13) != -1
-
-		country = receivedMsg[14:]
-		country = country.strip()
-
-		r = request.get('https://coronavirus-19-api.herokuapp.com/countries/' + country)
-
-		if r.status_code = 200
-			data = json.loads(r)
-			msgText = 'Country: ' +data["country"] + '\nTotal Cases: ' +data["cases"] + '\nCases Today: ' +data["todayCases"] + '\n\nTotal Deaths' +data["deaths"]+ '\nDeaths Today' +data["todayDeaths"]+ '\nRecovered:' + data["recovered"] 		
-			message.body(msgText)
-		else
-			message.body('Service unavailable at this time')
-
-		responded = True
-
-
-	if responded == False
-		return
-
-	return str(response)
 
 
 if __name__ == "__main__":
